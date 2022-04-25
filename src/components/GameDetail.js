117b8fbee3db9bb1f1d6b842f8e53e7f90e0d13e
tarak829 <abhishektarak45@gmail.com>
2022-04-25 23:23:12 +0530

    game rating added

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -4,6 +4,18 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { smallImage } from "../util";
+import PlayStation from "../img/playstation.svg";
+import Xbox from "../img/xbox.svg";
+import MacOS from "../img/apple.svg";
+import iOS from "../img/ios.svg";
+import Android from "../img/android.svg";
+import Windows from "../img/windows.svg";
+import Linux from "../img/linux.svg";
+import Gamepad from "../img/gamepad.svg";
+import Nintendo from "../img/nintendo.svg";
+import halfStar from "../img/star-half.svg";
+import fullStar from "../img/star.svg";
+import emptyStar from "../img/star-empty.svg";
 
 const GameDetail = ({ pathId }) => {
   const { screen, game, isLoading } = useSelector((state) => state.detail);
@@ -19,6 +31,70 @@ const GameDetail = ({ pathId }) => {
     }
   };
 
+  const gamePlatforms = (platforms) => {
+    let platformList = [];
+    platforms.map((platform) => {
+      return platformList.push(platform.platform.name);
+    });
+    // check if playstation 4 and playstation 5 are in the list
+    if (platformList.includes("PlayStation 4") && platformList.includes("PlayStation 5")) {
+      platformList.splice(platformList.indexOf("PlayStation 4"), 1);
+    }
+    // check if Xbox Series S/X and Xbox One are in the list
+    if (platformList.includes("Xbox Series S/X") && platformList.includes("Xbox One")) {
+      platformList.splice(platformList.indexOf("Xbox Series S/X"), 1);
+    }
+    // check if Nintendo Switch and Nintendo 3DS are in the list
+    if (platformList.includes("Nintendo Switch") && platformList.includes("Nintendo 3DS")) {
+      platformList.splice(platformList.indexOf("Nintendo 3DS"), 1);
+    }
+
+    return platformList;
+  };
+
+  const getStar = (rating) => {
+    let star = [];
+    for (let i = 1; i <= rating; i++) {
+      star.push(<img src={fullStar} alt="full star" />);
+    }
+    if (rating % 1 !== 0) {
+      star.push(<img src={halfStar} alt="half star" />);
+    }
+    for (let i = 1; i <= 5 - rating; i++) {
+      star.push(<img src={emptyStar} alt="empty star" />);
+    }
+    return star;
+  };
+
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return PlayStation;
+      case "PlayStation 5":
+        return PlayStation;
+      case "Xbox One":
+        return Xbox;
+      case "Xbox Series S/X":
+        return Xbox;
+      case "Nintendo Switch":
+        return Nintendo;
+      case "Nintendo 3DS":
+        return Nintendo;
+      case "iOS":
+        return iOS;
+      case "macOS":
+        return MacOS;
+      case "Linux":
+        return Linux;
+      case "Android":
+        return Android;
+      case "PC":
+        return Windows;
+      default:
+        return Gamepad;
+    }
+  };
+
   return (
     <>
       {!isLoading && (
@@ -28,13 +104,14 @@ const GameDetail = ({ pathId }) => {
               <div className='rating'>
                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
+                <div className='star'>{getStar(game.rating)}</div>
               </div>
               <Info>
                 <h3>Platforms</h3>
                 <Platforms>
-                  {game.platforms.map((data) => {
-                    return <h3 key={data.platform.id}>{data.platform.name}</h3>;
-                  })}
+                  {gamePlatforms(game.platforms).map((platform) => (
+                    <img src={getPlatform(platform)} alt={getPlatform(platform)} />
+                  ))}
                 </Platforms>
               </Info>
             </Stats>
@@ -101,6 +178,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  .star {
+    width: 1.5rem;
+    display: flex;
+  }
 `;
 
 const Info = styled(motion.div)`
@@ -109,9 +191,9 @@ const Info = styled(motion.div)`
 
 const Platforms = styled(motion.div)`
   display: flex;
-  justify-content: space-evenly;
-  img {
-    margin-left: 3rem;
+  align-items: center;
+  img:not(:last-child) {
+    margin-right: 2rem;
   }
 `;
 
